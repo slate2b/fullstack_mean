baab9763e0eaf27ccb36e8107e69319a16744075
slate2b <thomas.vaughn@gmail.com>
2023-03-15 12:56:43 -0700

    Converting Static HTML to Templates with JSON

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,10 @@
+var fs = require('fs'); 
+
+var foods = JSON.parse(fs.readFileSync('./data/foods.json', 'utf8'));
+
 /* GET meals view */
 const meals = (req, res) => {
-    res.render('meals', {title: 'Travlr Getaways'});
+    res.render('meals', {title: 'Travlr Getaways', foods});
 };
 
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,10 @@
+var fs = require('fs'); 
+
+var accommodations = JSON.parse(fs.readFileSync('./data/accommodations.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', {title: 'Travlr Getaways'});
+    res.render('rooms', {title: 'Travlr Getaways', accommodations});
 };
 
 module.exports = {
